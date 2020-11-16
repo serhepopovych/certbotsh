serhepopovych/certbotsh

commit 35dbd882f2a1e97f3131fb6b499cb8ed7b454451

certbotsh: Implement installer() update option
After installing updated version of certbot certain files and/or
directories might change ownership and/or permissions to default making
certbot fail to run under $runas user due to missing read/write bits.

Introduce new installer option "update" to help address above issues
after certbot update with single run of /usr/local/lib/certbot.sh update
that would fix permissions, symlinks and reinstall default config files
when they missing.

Later can be used to fix installation after assidential deletion of
configuration files (e.g. lighttpd.conf) or symlinks.

Note that certbot dns-rfc2136 plugin isn't patched as part of update
process and therefore users should patch and run /usr/bin/certbot -h all
to recompile .pyo/.pyc files manually.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -297,15 +297,17 @@ installer()
                     o="$(file_owner_human "$out")" &&
                     chown "$o" "$t" || rc=$?
                 fi
-                if [ $rc -eq 0 ]; then
+                while [ $rc -eq 0 ]; do
                     if [ -L "$out" ] || [ -e "$out" -a ! -f "$out" ]; then
                         rm -f "$out" ||:
                     fi
                     if [ -e "$out" ]; then
+                        [ -z "${put__skip_existing-}" ] || break
                         out="$out.certbotsh-new"
                     fi
                     mv -f "$t" "$out" || rc=$?
-                fi
+                    break
+                done
             fi
         else
             rc=$?
@@ -1267,6 +1269,7 @@ EOF
         new "$t" "$runas" "$runas" 0400
     } # update_cfg
 
+    local update='' put__skip_existing
     local homedir configdir cachedir extradir htdocsdir
     local t r
     local sudoers_line_runas sudoers_line_root sudoers_progs
@@ -1276,7 +1279,7 @@ EOF
     {
         local rc=$?
         cat >&2 <<EOF
-usage: [vars] $prog_name install [client|server|all]
+usage: [vars] $prog_name {install [client|server|all] | update}
 
 If argument to install is not specified 'client' is assumed.
 
@@ -1298,38 +1301,53 @@ EOF
         exit $rc
     }
 
-    [ $# -le 2 ] || usage
-    [ "${1-}" = 'install' ] || usage
-
-    case "${2-}" in
-        'all')
-            set -- "$1" '|client|server|'
-            ;;
-        '')
-            set -- "$1" '|client|'
-            ;;
-        'client'|'server')
-            set -- "$1" "|$2|"
-            ;;
-        *)
-            ! : || usage
-            ;;
-    esac
-
-    if [ x$(id -u) != x0 ]; then
-        echo >&2 "$prog_name: must run install as uid 0"
-        exit 1
-    fi
-
     if [ ! -f "$this" ]; then
         echo >&2 "$prog_name: \"$this\" is not found (read from stdin or -c?)"
         exit 1
     fi
 
-    if [ "$this" -ef "$install_to" ]; then
-        echo >&2 "$prog_name: already installed"
-        exit 0
+    if [ x$(id -u) != x0 ]; then
+        echo >&2 "$prog_name: must run as uid 0"
+        exit 1
     fi
+
+    case "${1-}" in
+        'install')
+            [ $# -le 2 ] || usage
+
+            case "${2-}" in
+                'all')
+                    set -- "$1" '|client|server|'
+                    ;;
+                '')
+                    set -- "$1" '|client|'
+                    ;;
+                'client'|'server')
+                    set -- "$1" "|$2|"
+                    ;;
+                *)
+                    ! : || usage
+                    ;;
+            esac
+
+            if [ "$this" -ef "$install_to" ]; then
+                echo >&2 "$prog_name: already installed"
+                exit 0
+            fi
+            ;;
+        'update')
+            update='1'
+
+            if [ ! "$this" -ef "$install_to" ]; then
+                echo >&2 "$prog_name: not installed. Hint: run '$install_to'."
+                exit 0
+            fi
+            ;;
+        *)
+            ! : || usage
+            ;;
+    esac
+    put__skip_existing="$update"
 
     # Sudoers(5) template
     sudoers_line_runas="%$certmgr ALL=($runas:$runas) SETENV:NOPASSWD: "
@@ -1376,8 +1394,13 @@ EOF
     }
     eval '_usermod "$runas" "$homedir"'
 
-    # Install self
-    install -Dpm0750 -g "$certmgr" "$this" "$install_to"
+    if [ -z "$update" ]; then
+        # Install self
+        install -D -m 0750 -p -g "$certmgr" "$this" "$install_to"
+    else
+        # Update ownership and permissions on self
+        new "$install_to" 'root' "$certmgr" 0750
+    fi
 
     # Working directory
     t='/etc/letsencrypt/' && new "$t"

commit 4f05003ec61e108034acc8751ef3eaa67ea8f6c0

certbotsh: Fix unbound variable access if install invoked for update
In most places installer() assumes that at least two options passed
as parameter on comment line. This isn't true when invoked with update
option that does not assume any options to be passed by user and thus
shell throws unbound variable access error (enabled with set -u to
catch such cases).

Fix by forcibly injecting '|update|' as second argument to installer().

While there simplify code a bit and get rid of local update='' variable.

Fixes: 35dbd882f2a1 ("certbotsh: Implement installer() update option")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -1269,7 +1269,7 @@ EOF
         new "$t" "$runas" "$runas" 0400
     } # update_cfg
 
-    local update='' put__skip_existing
+    local put__skip_existing=''
     local homedir configdir cachedir extradir htdocsdir
     local t r
     local sudoers_line_runas sudoers_line_root sudoers_progs
@@ -1342,12 +1342,14 @@ EOF
                 echo >&2 "$prog_name: not installed. Hint: run '$install_to'."
                 exit 0
             fi
+
+            set -- "$1" '|update|'
+            put__skip_existing="$2"
             ;;
         *)
             ! : || usage
             ;;
     esac
-    put__skip_existing="$update"
 
     # Sudoers(5) template
     sudoers_line_runas="%$certmgr ALL=($runas:$runas) SETENV:NOPASSWD: "
@@ -1394,7 +1396,7 @@ EOF
     }
     eval '_usermod "$runas" "$homedir"'
 
-    if [ -z "$update" ]; then
+    if [ "$2" != 'update' ]; then
         # Install self
         install -D -m 0750 -p -g "$certmgr" "$this" "$install_to"
     else