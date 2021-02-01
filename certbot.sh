serhepopovych/certbotsh

commit e8e002ec811efb0cc07c755ccd0513e089fdd66c

certbotsh: Use correct statement to return from function
In certbotsh-hook do_hook() use correct statement to return failure
if service name contains unsupported for shell variable symbols.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -2319,8 +2319,8 @@ applet()
             s="${s%@*}"
 
             # Skip invalid shell variable names
-            [ -n "${s##[0-9]*}" ] || continue
-            [ -n "${s##*[^[:alnum:]_]*}" ] || continue
+            [ -n "${s##[0-9]*}" ] || return
+            [ -n "${s##*[^[:alnum:]_]*}" ] || return
 
             eval "local fn=\"\${${s}_${h}-}\""
             [ -z "$fn" ] || "$fn" "$@" || return

commit cd6005c7dcd994eb4e48ff8a175c1e52895df476

certbotsh: Report success when service has invalid shell symbols
Follow behaviour when service specific helper code isn't available and
report success to the caller. Otherwise we fail to restart service.

Fixes: e8e002ec811e ("certbotsh: Use correct statement to return from function")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -2320,8 +2320,8 @@ applet()
             s="${s%@*}"
 
             # Skip invalid shell variable names
-            [ -n "${s##[0-9]*}" ] || return
-            [ -n "${s##*[^[:alnum:]_]*}" ] || return
+            [ -n "${s##[0-9]*}" ] || return 0
+            [ -n "${s##*[^[:alnum:]_]*}" ] || return 0
 
             eval "local fn=\"\${${s}_${h}-}\""
             [ -z "$fn" ] || "$fn" "$@" || return