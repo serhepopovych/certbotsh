serhepopovych/certbotsh

commit bec320119a7eab82c656463ee08e8477226bce68

certbotsh: Strictly check for errors while installing new data
It still does not solve nesting issues observed on deployments where
contents of $t ($domain) not owned by $runas user (e.g. default
letsencrypt user), so rm -rf "$td" in exit_handler() cannot remove it
causing directory nesting on each run, but make sure errors catched at
each stage of new data installation process.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -1833,11 +1833,13 @@ applet()
         done
 
         # Install new (temporary) data
-        mv -f "$t" "$td" 2>/dev/null ||:
-        if mv -f "$td" "$t"; then
-            rm -rf "$t/$domain" && chmod 0755 "$t" && td='' ||:
-        else
-            mv -f "$td/$domain" "$t" 2>/dev/null ||:
+        if mv -f "$t" "$td"; then
+            if mv -f "$td" "$t"; then
+                chmod 0755 "$t" ||:
+                td="$t/$domain"
+            else
+                mv -f "$td/$domain" "$t" ||:
+            fi
         fi
 
         ## Leave renewed certificate lineage

commit 5d3986e12c1ec8b20cc051aabbee94d06ecb9861

certbotsh: Respect new certificates installation
For new certificates that does not have $t ($domain) directory mv(1)
fails preventing deployment. Skip mv(1) if $t does not exist to con-
tinue new certificate deployment.

Fixes: bec320119a7e ("certbotsh: Strictly check for errors while installing new data")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

certbot.sh
@@ -1833,7 +1833,7 @@ applet()
         done
 
         # Install new (temporary) data
-        if mv -f "$t" "$td"; then
+        if ! [ -e "$t" ] || mv -f "$t" "$td"; then
             if mv -f "$td" "$t"; then
                 chmod 0755 "$t" ||:
                 td="$t/$domain"